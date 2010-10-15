4748293301d17aefdac7621fcb5967522b3df583
John Gordon <jgor@indiecom.org>
2010-10-15 20:58:06 +0000

    formatting

--- src/ss.h
+++ src/ss.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2010 John Gordon <jgor@indiecom.org> and Doug Farre <dougfarre@gmail.com>
+ * Copyright (c) 2010 John Gordon <jgor@indiecom.org>
+ *   and Doug Farre <dougfarre@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
